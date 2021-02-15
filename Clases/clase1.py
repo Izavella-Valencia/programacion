7ffb499bde299c19eb384ea3dece4467d16933b8
Izavella-Valencia <valencia.izavella@uces.edu.co>
2021-02-15 12:54:51 -0500

    talleres

--- Clases/clase1.py
+++ Clases/clase1.py
@@ -1,5 +1,8 @@
-
+#se utiliza string para palabras 
 saludo = "Hola mundo"
+#utilizamos Int para numeros enteros 
 edad = 18
+#y utilizamos float para decimales 
 estatura = 1,60
+#print para ver en pantalla
 print(saludo, edad, estatura)
